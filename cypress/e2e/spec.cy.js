9fcc2c97be5f22fa577cf1a288c568c9807f14fd
Kyle Jensen <kjensen19@gmail.com>
2023-01-19 22:30:54 -0600

    added cypress and basic passing and failing test examples (specs)

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+//Passing test:
+// describe('My First Test', () =>{
+//   it('Does not do much!', () =>{
+//     expect(true).to.equal(true)
+//   })
+// })
+
+//Failing test:
+// describe('My First Test', () => {
+//   it('Does not do much!', () => {
+//     expect(true).to.equal(false)
+//   })
+// })
+
+// A solid test generally covers 3 phases:
+
+// Set up the application state.
+// Take an action.
+// Make an assertion about the resulting application state.
+
+//Sometimes Given => When => Then
+//Or
+//Arrange => Act => Assert
+
