259b07ca4b284a3ec8a9b34ebb9651f6248bd239
Kyle Jensen <kjensen19@gmail.com>
2023-01-19 22:44:13 -0600

    added one full cypress test spec example for cypress page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -22,3 +22,31 @@
 //Or
 //Arrange => Act => Assert
 
+//test that actually visits a url
+// describe('My First Test', () => {
+//   it('Visits the Kitchen Sink', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+//Find content "type"
+//uses default assertion (built to fail if it doesn't find something)
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+    //Adding click does the expected
+    cy.contains('type').click()
+    //Should be on a new url which includes
+    //'/commands/actions'
+    cy.url().should('include', '/commands/actions')
+    //Get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+    //verify that the value updated
+    cy.get('.action-email').should('have.value', 'fake@email.com')
+  })
+})
+
+// Cypress automatically detects things like a 
+// page transition event and will automatically 
+// halt running commands until the next page 
+// has finished loading.
